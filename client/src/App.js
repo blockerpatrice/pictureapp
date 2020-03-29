e4fab6ce689d433c4327258a98168f61b28d3465
blockerpatrice <patricemblocker@yahoo.com>
2020-03-29 11:16:33 -0600

    Project for Heroku

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import MyNavbar from "./navbar/MyNavbar.js";
+import Home from "./homepage/Home.js";
+import SearchPage from "./search/SearchPage.js";
+
+
+
+function App () {
+    return(
+        <div>
+            <MyNavbar />
+            <Switch>
+                <Route exact path="/" component={Home}/>
+              
+                <Route path="/search" component={SearchPage} />
+              
+            </Switch>
+        </div>
+    )
+}
+
+export default App;
