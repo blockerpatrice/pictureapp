48567a2d07233254ed743b8176b0903036ce9106
blockerpatrice <patricemblocker@yahoo.com>
2020-03-29 11:44:48 -0600

    still tweaking for heroku

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const mongoose = require("mongoose");
 //const expressJwt = require("express-jwt");
 
 app.use(express.json());
-
+const path = require("path")
 //app.use("/", require("./userRouter"));
 
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-mongoose.connect('mongodb://localhost:27017/picture-app',{useNewUrlParser: true})
-.then(()=> console.log("Connected to MongoDB"))
-.catch(err => console.error(err));
+
+// mongoose.connect('mongodb://localhost:27017/picture-app',{useNewUrlParser: true})
+// .then(()=> console.log("Connected to MongoDB"))
+// .catch(err => console.error(err));
 
 app.use((err, req, res, next) => {
     console.error(err);
@@ -22,10 +24,15 @@ app.use((err, req, res, next) => {
     }
     return res.send({ message: err.message });
  });
+
+ app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+ });
  
  //app.use("/auth", require("./routes/auth"));
  //app.use("/api", expressJwt({secret: process.env.SECRET}));
  
- app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`);
- });
